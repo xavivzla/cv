2c1a4dc52def6db00fb83b592c4c726da44413fe
·Xavi· ~ <xavivzla.g@gmail.com>
2020-02-01 22:23:29 -0500

    Updates

---
+++ precache-manifest.7d0881a9c33b8bd29415e2021e10c8a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46956a725909979afbded257ee31d9f7",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "e3a5d600a6459b419295",
+    "url": "/cv/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c7c099ff21a00cbde2d",
+    "url": "/cv/static/js/2.2ca6605c.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/cv/static/js/2.2ca6605c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a5d600a6459b419295",
+    "url": "/cv/static/js/main.6ac57361.chunk.js"
+  },
+  {
+    "revision": "a9cad12f4b2563766723",
+    "url": "/cv/static/js/runtime-main.29273adb.js"
+  }
+]);
